fa2d5e81afec96b3e52823aa9477fb9f6bf36fdc
tiagojc06 <tiagojcastelo@gmail.com>
2025-01-07 18:22:04 -0300

    estos archivos estan listos para produccion

--- src/index.js
+++ src/index.js
@@ -18,5 +18,5 @@ app.get("/about", (req, res) => {
   res.send("<h1>About</h1>");
 })
 
-app.listen(3000);
-console.log("Server on port", 3000);
+app.listen(process.env.PORT || 3000);
+console.log("Server on port", process.env.PORT || 3000);
